c51e16b64907dcc8bbc6ce796327b8a16b895767
Raghav Verma <91414579+Raghav-Verma25@users.noreply.github.com>
2022-08-06 23:42:36 +0530

    Create fabonacci.java

---
+++ fabonacci.java
@@ -0,0 +1,18 @@
+public class fabonacci {
+    public static void fab(int a , int b , int n){
+        if(n==0){
+            return;
+        }
+        int c= a + b;
+        System.out.println(c);
+        fab(b,c,n-1);
+    }
+    public static void main(String[] args) {
+        int a = 0;
+        int b = 1;
+        System.out.println(a);
+        System.out.println(b);
+        int n=7;
+        fab(a,b,n-2);
+    }
+}
